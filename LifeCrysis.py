0bfb184c64a057d47cfbc7614c8defc65d04830f
lukeskehoe <45520869+lukeskehoe@users.noreply.github.com>
2021-01-10 16:46:17 +0000

    clean-up
    clean code

--- LifeCrysis.py
+++ LifeCrysis.py
@@ -6,7 +6,7 @@ def get_age_in_hours(birthday: object) -> object:
 #    datetime_now = datetime.date.today()
     day, month, year = list(map(int, birthday.split("/")))
     birth_date = datetime.date(year, month, day)
-    print(f"Birthday is on {birth_date.strftime('%d/%m/%Y')}")
+#    print(f"Birthday is on {birth_date.strftime('%d/%m/%Y')}")
 
     # returns a timedelta object
     diff = datetime_now - birth_date
@@ -19,9 +19,10 @@ def get_age_in_hours(birthday: object) -> object:
 
 def get_wake_hours(age):
 
-    average_waking_hours = 16/24
-    wake_hours = age*average_waking_hours
-    return wake_hours
+    sleep = 8 # TODO will change to average sleep per age demographic
+    average_waking_hours = (24-sleep)/24
+    hours_waking = age*average_waking_hours
+    return hours_waking
 
 
 def hours_played_percentage(hours, age_in_hours):
@@ -32,18 +33,17 @@ def hours_played_percentage(hours, age_in_hours):
 
 def waking_hours_played_percentage(hours, age_in_hours):
 
-    wake_hours = get_wake_hours(age_in_hours)
-    percentage_of_life = (hours/wake_hours)*100
+    hours_awake = get_wake_hours(age_in_hours)
+    percentage_of_life = (hours/hours_awake)*100
     return percentage_of_life
 
 def get_hours_since_start(activity_start_date):
-
 
     day, month, year = list(map(int, activity_start_date.split("/")))
     start_date = datetime.date(year,month,day)
     diff = datetime_now - start_date
     hours = diff.total_seconds() / 60 / 60
-    print('hours since  date: ', hours)
+#    print('Hours since date: ', hours)
 
     return hours
 
@@ -51,11 +51,11 @@ def get_hours_since_start(activity_start_date):
 datetime_now = datetime.date.today()
 
 
-#activity_start_date = input('Enter when you started doing the activity dd/mm/yyyy format: ')
-activity_start_date = "09/01/2021"
+activity_start_date = input('Enter when you started doing the activity dd/mm/yyyy format: ')
+#activity_start_date = "09/01/2021"
 hours_since_start = get_hours_since_start(activity_start_date)
-#activity_hours_played = float(input("Enter activity hours: "))
-activity_hours_played = 1
+activity_hours_played = float(input("Enter activity hours: "))
+#activity_hours_played = 1
 
 
 percent_since_start = hours_played_percentage(activity_hours_played, hours_since_start)
